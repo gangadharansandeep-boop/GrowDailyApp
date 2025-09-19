665a5388f753218454d9d3f2c04ba55c740100bd
Divya D <gangadharan.sandeep@gmail.com>
2025-09-19 19:53:23 +0800

    Updates to splash screen, icon and launch

--- app/src/main/java/com/divyadiv/growdailyapp/MainActivity.kt
+++ app/src/main/java/com/divyadiv/growdailyapp/MainActivity.kt
@@ -1,17 +1,14 @@
 package com.divyadiv.growdailyapp
 
 import android.content.Context
+import android.graphics.Color // Added import
 import android.os.Bundle
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import java.util.UUID
 
-/**
- * Main activity for the GrowDailyApp.
- * This activity hosts a WebView to display the GrowDaily streamlit app,
- * persisting a unique user ID across sessions.
- */
 class MainActivity : AppCompatActivity() {
 
     companion object {
@@ -19,16 +16,9 @@ class MainActivity : AppCompatActivity() {
         private const val USER_ID_KEY = "UserId"
     }
 
-    /**
-     * Called when the activity is first created.
-     *
-     * This method initializes the activity, sets the content view, configures the WebView,
-     * and ensures a persistent unique ID is used for the web session.
-     * @param savedInstanceState If the activity is being re-initialized after
-     *     previously being shut down then this Bundle contains the data it most
-     *     recently supplied in [onSaveInstanceState].  <b><i>Note: Otherwise it is null.</i></b>
-     */
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
@@ -36,6 +26,9 @@ class MainActivity : AppCompatActivity() {
         webView.webViewClient = WebViewClient()
         webView.settings.javaScriptEnabled = true
         webView.settings.domStorageEnabled = true
+
+        // Make WebView background transparent until web page loads
+        webView.setBackgroundColor(Color.TRANSPARENT)
 
         val sharedPreferences = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
         var userId = sharedPreferences.getString(USER_ID_KEY, null)
